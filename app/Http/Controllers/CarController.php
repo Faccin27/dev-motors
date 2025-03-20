28da447beae58b369f92b37b0ce264b865e9ac25
Guilherme Faccin <gfaccin27@gmail.com>
2025-03-20 18:14:49 -0300

    first commit

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    function teste(){
+        return view('admin.login');
+    }
+}
