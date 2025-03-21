d24a06f7a63ac12b25035c873b170463394f2cd8
Guilherme Faccin <gfaccin27@gmail.com>
2025-03-21 12:57:53 -0300

    feat: created all routes

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,33 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarController;
 
-Route::get('/', [CarController::class, 'teste']);
+// Rotas do Site
+Route::get('/', [App\Http\Controllers\Site\HomeController::class, 'index'])->name('site.home');
+Route::get('/veiculo/{id}', [App\Http\Controllers\Site\VeiculoController::class, 'show'])->name('site.veiculo.show');
+Route::post('/lead', [App\Http\Controllers\Site\LeadController::class, 'store'])->name('site.lead.store');
+
+// Rotas para AJAX
+Route::get('/modelos/{marca_id}', function($marca_id) {
+    return App\Models\Modelo::where('marca_id', $marca_id)->get();
+})->name('api.modelos');
+
+// Rotas do Admin
+Route::prefix('admin')->name('admin.')->group(function() {
+    // Dashboard
+    Route::get('/', function() {
+        return view('admin.dashboard');
+    })->name('dashboard');
+    
+    // Marcas
+    Route::resource('marcas', App\Http\Controllers\Admin\MarcaController::class);
+    
+    // Modelos
+    Route::resource('modelos', App\Http\Controllers\Admin\ModeloController::class);
+    
+    // Veículos
+    Route::resource('veiculos', App\Http\Controllers\Admin\VeiculoController::class);
+    Route::delete('fotos/{id}', [App\Http\Controllers\Admin\VeiculoController::class, 'deleteFoto'])->name('fotos.delete');
+    
+    // Leads
+    Route::resource('leads', App\Http\Controllers\Admin\LeadController::class)->only(['index', 'show', 'destroy']);
+});
